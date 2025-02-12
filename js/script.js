f1d2e82ef93a1b11a77c766e4d7681e7f9a88f6b
omar_walied <zaza03455@gmail.com>
2025-02-12 15:09:11 +0200

    Add cards section with background between previous section and cards

--- js/script.js
+++ js/script.js
@@ -4,12 +4,3 @@ copyButtons.forEach((element) => {
     window.alert("Copied!");
   };
 });
-
-window.addEventListener("scroll", function () {
-  const navbar = document.querySelector(".transition-navbar");
-  if (window.scrollY > 10) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
-});
